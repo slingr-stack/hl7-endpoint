a12705d27d2c0918ba4cfdcb3b7aa07c172eecdf
Diego Gaviola <dgaviola@slingr.io>
2021-01-12 11:29:49 -0300

    fixing java version

--- src/main/java/io/slingr/endpoints/hl7/Hl7Endpoint.java
+++ src/main/java/io/slingr/endpoints/hl7/Hl7Endpoint.java
@@ -1,22 +1,13 @@
 package io.slingr.endpoints.hl7;
 
 import io.slingr.endpoints.Endpoint;
-import io.slingr.endpoints.autotask.ws.*;
-import io.slingr.endpoints.exceptions.EndpointException;
-import io.slingr.endpoints.exceptions.ErrorCode;
 import io.slingr.endpoints.framework.annotations.ApplicationLogger;
 import io.slingr.endpoints.framework.annotations.EndpointFunction;
 import io.slingr.endpoints.framework.annotations.EndpointProperty;
 import io.slingr.endpoints.framework.annotations.SlingrEndpoint;
 import io.slingr.endpoints.services.AppLogs;
 import io.slingr.endpoints.utils.Json;
-import io.slingr.endpoints.ws.exchange.FunctionRequest;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-
-import javax.xml.soap.SOAPException;
-import java.util.ArrayList;
-import java.util.List;
 
 @SlingrEndpoint(name = "hl7")
 public class Hl7Endpoint extends Endpoint {
@@ -42,10 +33,6 @@ public class Hl7Endpoint extends Endpoint {
 
     @EndpointProperty
     private String entitiesToPoll;
-
-    private AutotaskApi autotaskApi;
-
-    private PollingService pollingService;
 
     public Hl7Endpoint() {
     }
