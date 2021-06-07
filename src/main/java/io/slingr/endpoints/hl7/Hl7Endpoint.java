7fe829fc48f9b528468e49125dd5935c7dcfecef
Nicolas Marquez <nicolas_marquez10@hotmail.com>
2021-06-07 16:37:06 -0300

    Build of ADT_01 from JSON

--- src/main/java/io/slingr/endpoints/hl7/Hl7Endpoint.java
+++ src/main/java/io/slingr/endpoints/hl7/Hl7Endpoint.java
@@ -1,12 +1,20 @@
 package io.slingr.endpoints.hl7;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import ca.uhn.hl7v2.model.v281.segment.EVN;
+import ca.uhn.hl7v2.model.v281.segment.MSH;
+import ca.uhn.hl7v2.model.v281.segment.PID;
+import ca.uhn.hl7v2.model.v281.segment.PV1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +28,8 @@ import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.parser.Parser;
 import ca.uhn.hl7v2.protocol.ReceivingApplication;
 import ca.uhn.hl7v2.protocol.ReceivingApplicationException;
+import ca.uhn.hl7v2.model.v281.message.ADT_A01;
+
 import io.slingr.endpoints.Endpoint;
 import io.slingr.endpoints.framework.annotations.ApplicationLogger;
 import io.slingr.endpoints.framework.annotations.EndpointConfiguration;
@@ -166,9 +176,111 @@ public class Hl7Endpoint extends Endpoint {
 		}
 		return responseString;
 	}
+
+	@EndpointFunction(name = "_sendHl7AsJSON")
+	public String sendHl7AsJSON(Json params) throws Exception {
+		String responseString = "";
+
+		switch (params.string("messageType").toUpperCase()) {
+			case "ACK":
+				break;
+			case "ADT":
+				responseString = buildADTmessage(params);
+				break;
+			case "BAR":
+				break;
+			case "DFT":
+				break;
+			case "MDM":
+				break;
+			case "MFN":
+				break;
+			case "ORM":
+				break;
+			case "ORU":
+				break;
+			case "QRY":
+				break;
+			case "RAS":
+				break;
+			case "RDE":
+				break;
+			case "RGV":
+				break;
+			case "SIU":
+				break;
+			default:
+				responseString = "No messageType specified";
+		}
+
+		return responseString;
+	}
+
+	private String buildADTmessage(Json params) throws HL7Exception, IOException {
+		String encodedMessage;
+		switch (params.string("triggerEvent").toUpperCase()) {
+			case "A01":
+				ADT_A01 adt = new ADT_A01();
+				adt.initQuickstart("ADT", "A01", "P");
+
+				// Populate the MSH Segment
+				MSH mshSegment = adt.getMSH();
+				mshSegment.getSendingApplication().getNamespaceID().setValue("TestSendingSystem");
+				mshSegment.getSendingFacility().getNamespaceID().setValue("TestSendingFacility");
+				mshSegment.getReceivingApplication().getNamespaceID().setValue(params.string("receivingApplication").isEmpty() ? "TestReceivingSystem" : params.string("receivingApplication"));
+				mshSegment.getReceivingFacility().getNamespaceID().setValue(params.string("receivingApplication").isEmpty() ? "TestReceivingFacility" : params.string("receivingFacility"));
+				mshSegment.getSequenceNumber().setValue("123");
+
+				//Populate the EVN Segment
+				EVN evnSegment = adt.getEVN();
+				evnSegment.getRecordedDateTime().setValue(new Date());
+
+				// Populate the PID Segment
+				PID pid = adt.getPID();
+				//THIS SHOULD BE ITERATED, BECAUSE THE PATIENT MAY HAVE SEVERAL NAMES
+				//for (Json patientName : params.string("patientNames") {
+					pid.getPatientIdentifierList(0).getIDNumber().setValue(params.string("patientID"));
+					pid.getPatientName(0).getFamilyName().getSurname().setValue(params.string("patientLastName"));
+					pid.getPatientName(0).getGivenName().setValue(params.string("patientFirstName"));
+					pid.getPatientName(0).getSecondAndFurtherGivenNamesOrInitialsThereof().setValue(params.string("patientOtherNames"));
+					pid.getPatientName(0).getSuffixEgJRorIII().setValue(params.string("patientNameSuffix"));
+					pid.getPatientName(0).getPrefixEgDR().setValue(params.string("PatientNamePrefix"));
+					pid.getPatientName(0).getDegreeEgMD().setValue(params.string("PatientNameDegree"));
+					pid.getMotherSMaidenName(0).getFamilyName().getSurname().setValue(params.string("patientMothersMaidenLastName"));
+				//}
+					//Making conversion from String to LocalDateTime and from LocalDateTime to Date
+					pid.getDateTimeOfBirth().setValue(Date.from(LocalDateTime.parse(params.string("patientDateOfBirth")).atZone(ZoneId.systemDefault()).toInstant()));
+
+				// Populate the PV1 Segment
+				PV1 pv1 = adt.getPV1();
+				pv1.getPatientClass().getIdentifier().setValue(params.string("patientClass"));
+				pv1.getAssignedPatientLocation().getPointOfCare().getNamespaceID().setValue(params.string("pointOfCare"));
+				pv1.getAssignedPatientLocation().getRoom().getNamespaceID().setValue(params.string("room"));
+				pv1.getAssignedPatientLocation().getBed().getNamespaceID().setValue(params.string("bed"));
+				pv1.getAssignedPatientLocation().getFacility().getNamespaceID().setValue(params.string("facility"));
+				pv1.getAssignedPatientLocation().getBuilding().getNamespaceID().setValue(params.string("building"));
+				pv1.getAssignedPatientLocation().getFloor().getNamespaceID().setValue(params.string("floor"));
+				pv1.getAdmissionType().getIdentifier().setValue(params.string("admissionType"));
+				pv1.getPreadmitNumber().getIDNumber().setValue(params.string("preadmitNumber"));
+				pv1.getPreadmitNumber().getIdentifierTypeCode().setValue(params.string("preadmitNumberType"));
+
+				// Now, let's encode the message and look at the output
+				Parser parser = context.getPipeParser();
+				encodedMessage = parser.encode(adt);
+				break;
+			default:
+				encodedMessage = "triggerEvent is empty";
+				break;
+		}
+
+		System.out.println("Printing ER7 Encoded Message:");
+		System.out.println(encodedMessage);
+
+		return encodedMessage;
+	}
 }
 
-class Receiver implements ReceivingApplication {
+class Receiver implements ReceivingApplication<Message> {
 	private Events events;
 	private AppLogs appLogger;
 
